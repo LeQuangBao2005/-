ff233b1850f9a9f2403322767a2f98729bec65e3
HOME <HOME@MSI>
2024-09-23 20:58:23 +0700

    nop bai thiết kế giao diện swingkhh

--- src/main/java/gui/JNotepad.java
+++ src/main/java/gui/JNotepad.java
@@ -2,38 +2,58 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package gui;
+import java.awt.BorderLayout;
 import java.awt.Font;
+
+import java.awt.event.ActionEvent;
+
 import java.awt.event.KeyEvent;
-import javax.swing.JCheckBoxMenuItem;
+import java.io.FileInputStream;
+import java.io.IOException;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JToolBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.KeyStroke;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+
 /**
  *
  * @author ADMIN
  */
 public class JNotepad extends JFrame {
+
     private JMenuBar mBar;
-    private JMenu mFile, mEdit, mFormat, mView, mHelp,mZoom;
-    private JMenuItem itemNew, itemOpen, itemSave, itemSaveAs, itemPageSetup, itemPrint, itemExit, itemFont,
-            itemUndo, itemCut, itemCopy, itemPaste, itemDelete, itemSearch, itemFind, itemFindNext, itemFindPrevious,
-            itemReplace, itemGoto, itemSelectAll, itemTimeDate, itemStatusBar, itemZoomIn, itemZoomOut, itemRestore,itemStatus,
-            itemViewHelp, itemSendFeedback, itemAbout;
+    private JMenu mFile, mEdit, mFormat, mView, mZoom, mHelp;
+    private JMenuItem itemNew, itemOpen, itemSave, itemSaveAs, itemPageSetup, itemPrint, itemExit, itemFornt,
+            itemCut, itemUndo, itemCopy, ItemDelete, itemPaste, itemSearch, itemFind, itemFindNext, itemFindPrevios,
+            itemViewHelp, itemSendFeedback, itemAbout, itemReplace, itemGoto, itemSelecAll, itemTimeDate, itemZoom, itemDelete,
+            itemZoomOut, itemStatus, itemFindPrevious, itemZoomIn, itemFont, itemSelectAll, itemRestore;
     private JCheckBoxMenuItem itemWrap;
     private JTextArea txtEditor;
+    private JToolBar toolBar;
+    private JButton btNew, btSave, btOpen;
+    int size = 20;
+
     public JNotepad(String title) {
         super(title);
         createMenu();
         createGUI();
+        createToolBar();
+
+        processEvent();
         setSize(700, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
     }
+
     private void createMenu() {
         //tao thanh thuc don
         mBar = new JMenuBar();
@@ -52,42 +72,42 @@ public class JNotepad extends JFrame {
         mFile.add(itemPrint = new JMenuItem("Print..."));
         mFile.addSeparator();
         mFile.add(itemExit = new JMenuItem("Exit"));
-        
+
         //tao item va add vao menu edit
-        mEdit.add(itemUndo=new JMenuItem("Undo"));
+        mEdit.add(itemUndo = new JMenuItem("Undo"));
         mEdit.addSeparator();
-        mEdit.add(itemCut=new JMenuItem("Cut"));
-        mEdit.add(itemCopy=new JMenuItem("Copy"));
-        mEdit.add(itemPaste=new JMenuItem("Paste"));
-        mEdit.add(itemDelete=new JMenuItem("Delete"));
+        mEdit.add(itemCut = new JMenuItem("Cut"));
+        mEdit.add(itemCopy = new JMenuItem("Copy"));
+        mEdit.add(itemPaste = new JMenuItem("Paste"));
+        mEdit.add(itemDelete = new JMenuItem("Delete"));
         mFile.addSeparator();
-        mEdit.add(itemSearch=new JMenuItem("Search with Bing..."));
-        mEdit.add(itemFind=new JMenuItem("Find..."));
-        mEdit.add(itemFindNext=new JMenuItem("Find Next"));
-        mEdit.add(itemFindPrevious=new JMenuItem("Find Previous"));
-        mEdit.add(itemReplace=new JMenuItem("Replace..."));
-        mEdit.add(itemGoto=new JMenuItem("Go To..."));
+        mEdit.add(itemSearch = new JMenuItem("Search with Bing..."));
+        mEdit.add(itemFind = new JMenuItem("Find..."));
+        mEdit.add(itemFindNext = new JMenuItem("Find Next"));
+        mEdit.add(itemFindPrevious = new JMenuItem("Find Previous"));
+        mEdit.add(itemReplace = new JMenuItem("Replace..."));
+        mEdit.add(itemGoto = new JMenuItem("Go To..."));
         mEdit.addSeparator();
-        mEdit.add(itemSelectAll=new JMenuItem("Select All"));
-        mEdit.add(itemTimeDate=new JMenuItem("Time/Date"));
-        
+        mEdit.add(itemSelectAll = new JMenuItem("Select All"));
+        mEdit.add(itemTimeDate = new JMenuItem("Time/Date"));
+
         //tao item va add vao menu Format
-        mFormat.add(itemWrap=new JCheckBoxMenuItem("Word Wrap"));
-        mFormat.add(itemFont=new JMenuItem("Font..."));
-        
+        mFormat.add(itemWrap = new JCheckBoxMenuItem("Word Wrap"));
+        mFormat.add(itemFont = new JMenuItem("Font..."));
+
         //tao item va add vao menu View
-        mView.add(mZoom=new JMenu("Zoom"));
-        mZoom.add(itemZoomIn=new JMenuItem("Zoom In"));
-        mZoom.add(itemZoomOut=new JMenuItem("Zoom Out"));
-        mZoom.add(itemRestore=new JMenuItem("Restore Default Zoom"));
-        mView.add(itemStatus=new JMenuItem("Status Bar"));
-        
+        mView.add(mZoom = new JMenu("Zoom"));
+        mZoom.add(itemZoomIn = new JMenuItem("Zoom In"));
+        mZoom.add(itemZoomOut = new JMenuItem("Zoom Out"));
+        mZoom.add(itemRestore = new JMenuItem("Restore Default Zoom"));
+        mView.add(itemStatus = new JMenuItem("Status Bar"));
+
         //tao item va add vao menu View
-        mHelp.add(itemViewHelp=new JMenuItem("View Help"));
-        mHelp.add(itemSendFeedback=new JMenuItem("Send Feedback"));
+        mHelp.add(itemViewHelp = new JMenuItem("View Help"));
+        mHelp.add(itemSendFeedback = new JMenuItem("Send Feedback"));
         mHelp.addSeparator();
-        mHelp.add(itemAbout=new JMenuItem("About Notepad"));
-        
+        mHelp.add(itemAbout = new JMenuItem("About Notepad"));
+
         //tao to hop phim
         itemNew.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, KeyEvent.CTRL_DOWN_MASK));
         itemOpen.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, KeyEvent.CTRL_DOWN_MASK));
@@ -113,15 +133,81 @@ public class JNotepad extends JFrame {
         //gan thuc don vao cua so
         setJMenuBar(mBar);
     }
+
     public static void main(String[] args) {
         JNotepad notepad = new JNotepad("Demo Notepad");
         notepad.setVisible(true);
     }
+
     private void createGUI() {
         txtEditor = new JTextArea();
         JScrollPane scrollEditor = new JScrollPane(txtEditor);
         add(scrollEditor);
         txtEditor.setFont(new Font("Arial", Font.PLAIN, 20));
+
     }
-    
+
+    private void processEvent() {
+        itemZoomIn.addActionListener((ActionEvent e) -> {
+            size += 4;
+            txtEditor.setFont(new Font("Arial", Font.PLAIN, size));
+        });
+        itemZoomOut.addActionListener((ActionEvent e) -> {
+            size -= 4;
+            txtEditor.setFont(new Font("Arial", Font.PLAIN, size));
+        });
+        itemWrap.addActionListener((ActionEvent e) -> {
+            if (itemWrap.isSelected()) {
+                txtEditor.setLineWrap(true);
+            } else {
+                txtEditor.setLineWrap(false);
+            }
+        });
+        itemOpen.addActionListener((ActionEvent e) -> {
+            openFile();
+        });
+        itemZoomOut.addActionListener((ActionEvent e) -> {
+            saveFile();
+        });
+    }
+
+    private void openFile() {
+        JFileChooser dlgFile = new JFileChooser();
+        if (dlgFile.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+            try {
+                FileInputStream fis = new FileInputStream(dlgFile.getSelectedFile());
+                byte[] b = new byte[fis.available()];
+                fis.read(b);
+                txtEditor.setText(new String(b));
+            } catch (IOException ex) {
+                JOptionPane.showMessageDialog(this, "Loi doc file");
+            }
+        }
+    }
+
+    private void saveFile() {
+        JFileChooser dlgFile = new JFileChooser();
+        if (dlgFile.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+            try {
+                FileInputStream fos = new FileInputStream(dlgFile.getSelectedFile());
+
+                fos.close();
+
+            } catch (IOException ex) {
+                JOptionPane.showMessageDialog(this, "Loi doc file");
+            }
+        }
+
+    }
+
+    private void createToolBar() {
+        toolBar = new JToolBar();
+        toolBar.add(btNew = new JButton("new"));
+        toolBar.add(btOpen = new JButton("Open"));
+        toolBar.add(btSave = new JButton("Save"));
+        btNew.setIcon(new ImageIcon(this.getClass().getResource("/images/new.png")));
+        btOpen.setIcon(new ImageIcon(this.getClass().getResource("/images/open.png")));
+        btSave.setIcon(new ImageIcon(this.getClass().getResource("/images/save.png")));
+        add(toolBar, BorderLayout.NORTH);
+    }
 }
